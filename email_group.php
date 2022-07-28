ef14604a6cb36f34ee449dc20709c3939b66527c
selvatek <antoine@serin.net>
2022-07-28 17:12:41 +0200

    select and send to learndash groups

--- email_group.php
+++ email_group.php
@@ -1,7 +1,8 @@
+
 <?php
 
 /**
-* Plugin Name: send email to learndash groups
+* Plugin Name: Groups learndash
 * Plugin URI:https://www.selvatek.com
 * Description: Groups learndash
 * Author: Antoine Serin
@@ -27,34 +28,54 @@ add_action( 'admin_menu', 'wpdocs_register_my_custom_menu_page_mail' );
 /**
  * Display a custom menu page
  */
-
 function my_custom_menu_page_mail(){
-    echo '<h2>Letter Templates <a href="admin.php?page=letter-template&type=new">Add New</a></h2>';
-//     echo '<div id="wpbody-content"><div class="wrap"> <h1 class="wp-heading-inline">
-// Groups Learndash</h1></div></div>';  
-//     echo '<br>';
-//     $user = learndash_get_groups_user_ids("2633");
-//     foreach($user as $user){
-//     	$userData = get_user_by('id', $user);
-        
-//         // variables pour l'envoie d'email
-//         $to = $userData->user_email;
-//         $message = "test";
-//         $subject = "Some text in subject...";
-//         $headers = 'From: '. 'contact@hydronomie.fr' . "\r\n" .
-//         'Reply-To: ' . 'contact@hydronomie.fr' . "\r\n";
+    
+    echo '<div id="wpbody-content"><div class="wrap"> <h1 class="wp-heading-inline">
+Groups Learndash</h1></div></div>';  
+    echo '<br>';
+
+    
+    
+    echo '<form method="POST"><select name="myselect"> ';
+   $groups = learndash_get_groups(1);
    
+   foreach($groups as $groups){
+    $groupName = get_the_title($groups);
+//    echo $groupName . '<br>'; 
+   echo '<option value="'.$groups.'">'.$groupName.'</option>'; 
  
-//  //Here put your Validation and send mail
-//  $sent = wp_mail($to, $subject, $message, $headers);
-//        if($sent) {
-//        echo 'Envoyé a: ' . $userData->first_name . ' ' . $userData->last_name . ' (' . $userData->user_email . ')' . '<br />'; 
-//        }//message sent!
-//        else  {
-//         echo 'Erreur Envoi: ' . $userData->first_name . ' ' . $userData->last_name . ' (' . $userData->user_email . ')' . '<br />';
-//        }//message wasn't sent
-       
-// }
+}
+echo '</select><input name"submit" type="submit" value="Submit"></form>';
+if (isset($_POST['myselect'])) {
+    echo ($_POST['myselect']);
+    $sendgroups = ($_POST['myselect']);
+    echo '$sendgroups: ' . $sendgroups . '<br>';
+    send_to_group($sendgroups);
 }
 
- 
+}
+function send_to_group($group){
+$user = 0;
+$user = learndash_get_groups_user_ids($group);
+echo '<br>' . $group;
+foreach($user as $user){
+    
+    $userData = get_user_by('id', $user);
+    
+    // variables pour l'envoie d'email
+    $to = $userData->user_email;
+    $message = "test";
+    $subject = "Some text in subject...";
+    $headers = 'From: '. 'contact@hydronomie.fr' . "\r\n" .
+    'Reply-To: ' . 'contact@hydronomie.fr' . "\r\n";
+
+
+//Here put your Validation and send mail
+$sent = wp_mail($to, $subject, $message, $headers);
+   if($sent) {
+   echo 'Envoyé a: ' . $userData->first_name . ' ' . $userData->last_name . ' (' . $userData->user_email . ')' . '<br />'; 
+   }//message sent!
+   else  {
+    echo 'Erreur Envoi: ' . $userData->first_name . ' ' . $userData->last_name . ' (' . $userData->user_email . ')' . '<br />';
+   }//message wasn't sent
+}}
